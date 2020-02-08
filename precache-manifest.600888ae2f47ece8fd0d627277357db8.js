71a2d257f049902cdb8a327a1c637fce1af3a38b
vini84200 <www.vini84200@hotmail.com>
2020-02-07 23:36:41 -0300

    Updates

---
+++ precache-manifest.600888ae2f47ece8fd0d627277357db8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "884c2dda3264461f82d2d0f8eb0d2b1d",
+    "url": "/gdg-react/index.html"
+  },
+  {
+    "revision": "0fdd640c8e8e96ebc003",
+    "url": "/gdg-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "586dfdca536b3ac98398",
+    "url": "/gdg-react/static/js/2.8c0a2c33.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/gdg-react/static/js/2.8c0a2c33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fdd640c8e8e96ebc003",
+    "url": "/gdg-react/static/js/main.69d3d0f3.chunk.js"
+  },
+  {
+    "revision": "985cb1ca27937baba34e",
+    "url": "/gdg-react/static/js/runtime-main.1320300e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gdg-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
