2828c6ab752f2ab2fb561085739f43d99a6a1956
Vinicius José Fritzen <www.vini84200@hotmail.com>
2020-03-05 23:21:01 -0300

    Mudado para HashRouter para evitar erro no gh pages.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,55 @@
-import React from 'react';
-import 'rbx/index.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import "rbx/index.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 import {
-    faCheckSquare,
-    faCoffee,
-    faPlus,
-    faFilePdf,
-    faFileExport,
-    faFileUpload,
-    faList
-} from '@fortawesome/free-solid-svg-icons';
+  faCheckSquare,
+  faCoffee,
+  faPlus,
+  faFilePdf,
+  faFileExport,
+  faFileUpload,
+  faList
+} from "@fortawesome/free-solid-svg-icons";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
-import Header from './components/Header';
-import MocaoPage from './components/mocao/MocaoPage';
-import Ata from './components/atas/Ata'
-import PaginaInicial from './components/PaginaInicial';
+import Header from "./components/Header";
+import MocaoPage from "./components/mocao/MocaoPage";
+import Ata from "./components/atas/Ata";
+import PaginaInicial from "./components/PaginaInicial";
 
-library.add(fab, faCheckSquare, faCoffee, faList, faPlus, faFilePdf,faFileExport,faFileUpload);
+library.add(
+  fab,
+  faCheckSquare,
+  faCoffee,
+  faList,
+  faPlus,
+  faFilePdf,
+  faFileExport,
+  faFileUpload
+);
 function App() {
-    return (
-      <div className="App">
-      <Router>
-              <Header />
-              <Switch>
+  return (
+    <div className="App">
+      <Router basename="/">
+        <Header />
+        <Switch>
+          <Route path="/resolucao">
+            <MocaoPage />
+          </Route>
 
-                <Route path="/resolucao">
-                  <MocaoPage />
-                </Route>
+          <Route path="/ata">
+            <Ata />
+          </Route>
 
-                <Route path="/ata">
-                  <Ata />
-                </Route>
-
-                <Route path="/">
-                  <PaginaInicial />
-                </Route>
-
-              </Switch>
-
-        </Router>
-        </div>
-    );
+          <Route path="/">
+            <PaginaInicial />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
