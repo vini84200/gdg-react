8784373b0e584d5243fd7f0a02b2e42e855d0210
Vinicius Jose Fritzen <www.vini84200@hotmail.com>
2020-08-29 14:02:27 -0300

    Add the html-pdf generator

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const pug = require("pug");
+var pdf = require("html-pdf");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,6 +13,18 @@ app.get("/api/mensagem", (req, res) => {
 });
 
 app.get("/api/branco", (req, res) => {
+  const rendered = pug.renderFile("templates/base.pug", {
+    basedir: "templates",
+    author: "Timothy",
+  });
+  pdf.create(rendered).toBuffer((err, buffer) => {
+    console.log("This is a buffer:", Buffer.isBuffer(buffer));
+    res.writeHead(200, [["Content-Type", "application/pdf"]]);
+    res.end(buffer);
+  });
+});
+
+app.get("/api/branco_html", (req, res) => {
   const rendered = pug.renderFile("templates/base.pug", {
     basedir: "templates",
     author: "Timothy",
