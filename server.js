16a96cd3ce9766edc77d450f0bc1e41915dbbecb
Vinicius Jose Fritzen <www.vini84200@hotmail.com>
2020-08-29 14:20:14 -0300

    Usando puppeteer

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const pug = require("pug");
-var pdf = require("html-pdf");
+const puppeteer = require("puppeteer");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,15 +13,31 @@ app.get("/api/mensagem", (req, res) => {
 });
 
 app.get("/api/branco", (req, res) => {
-  const rendered = pug.renderFile("templates/base.pug", {
+  const html = pug.renderFile("templates/base.pug", {
     basedir: "templates",
     author: "Timothy",
   });
-  pdf.create(rendered).toBuffer((err, buffer) => {
-    console.log("This is a buffer:", Buffer.isBuffer(buffer));
-    res.writeHead(200, [["Content-Type", "application/pdf"]]);
-    res.end(buffer);
+  // pdf.create(rendered).toBuffer((err, buffer) => {
+  //   console.log("This is a buffer:", Buffer.isBuffer(buffer));
+  //   res.writeHead(200, [["Content-Type", "application/pdf"]]);
+  //   res.end(buffer);
+  // });
+  puppeteer.launch({ args: ["--no-sandbox"] }).then((browser) => {
+    browser.newPage().then((page) => {
+      page
+        .setContent(html)
+        .then(() => {
+          return page.pdf({ format: "A4" });
+        })
+        .then((buffer) => {
+          res.writeHead(200, [["Content-Type", "application/pdf"]]);
+          res.end(buffer);
+        })
+        .finally(() => {});
+    });
   });
+
+  // We use pdf function to generate the pdf in the same folder as this file.
 });
 
 app.get("/api/branco_html", (req, res) => {
