e784eeae74817f392dad7705e371baf810a765c5
Vinicius Jose Fritzen <www.vini84200@hotmail.com>
2020-08-29 12:53:41 -0300

    Creating HTML from template

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const morgan = require("morgan");
+const pug = require("pug");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(morgan("dev"));
+
 app.get("/api/mensagem", (req, res) => {
   res.send({ express: "Hello From Express" });
+});
+
+app.get("/api/branco", (req, res) => {
+  const rendered = pug.renderFile("templates/base.pug", {
+    basedir: "templates",
+    author: "Timothy",
+  });
+  res.send(rendered);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
